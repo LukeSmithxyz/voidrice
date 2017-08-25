beeb57846838282d6f1847253ec60b97d762cbdd
Luke <luke@lukesmith.xyz>
2017-08-24 21:56:52 -0700

    Non-descript fixes

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@
 
 xbps-install xorg-minimal xorg-fonts xf86-input-synaptics xf86-video-intel base-devel xinit xorg-server rxvt-unicode feh ffmpeg arandr mpv wget curl rofi python-pip python-netifaces python-psutil NetworkManager network-manager-applet vim w3m ranger mediainfo poppler highlight tmux calcurse htop newsbeuter moc firefox qutebrowser ImageMagick transmission-gtk transmission atool libcaca compton transset blender gimp texlive MultiMarkdown mupdf evince audacity rsync youtube-dl openssh syncthing noto-fonts-cjk noto-fonts-emoji cups screenFetch neofetch scrot unzip git lmms p7zip font-tamsyn speedometer neomutt font-awesome mypaint pandoc xdotool unclutter-xfixes
 
-tlmgr info collections | grep -o 'collection-[A-Za-z]*' | zargs tlmgr install
+tlmgr info collections | grep -o 'collection-[A-Za-z]*' | xargs tlmgr install
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
