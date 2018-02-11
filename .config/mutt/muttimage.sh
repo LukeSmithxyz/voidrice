cb78188e531905ae2e9522f9834b7e32cfbfc4fe
Luke <luke@lukesmith.xyz>
2018-02-11 15:08:58 -0700

    mutt stuff removed. See LukeSmithxyz/mutt-wizard

--- .config/mutt/muttimage.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
- 
-#### Determine size of Terminal
-height=`stty  size | awk 'BEGIN {FS = " "} {print $1;}'`
-width=`stty  size | awk 'BEGIN {FS = " "} {print $2;}'`
- 
-### Display Image / offset with mutt bar
-echo -e "2;3;\n0;1;210;20;$((width*7-250));$((height*14-100));0;0;0;0;$1\n4;\n3;" |  /usr/lib/w3m/w3mimgdisplay &
