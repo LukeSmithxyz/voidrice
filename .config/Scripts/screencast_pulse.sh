c43a2797422db256c17d6236eeb96ec905bd0cbb
RaitaroHikami <19552720+RaitaroH@users.noreply.github.com>
2018-02-16 20:10:54 +0200

    fix Past duration too large

--- .config/Scripts/screencast_pulse.sh
+++ .config/Scripts/screencast_pulse.sh
@@ -14,9 +14,11 @@ filename="$HOME/screencast$n.mkv"
 
 ffmpeg -y \
 -f x11grab \
+-framerate 60 \
 -s $(xdpyinfo | grep dimensions | awk '{print $2;}') \
 -i :0.0 \
 -f alsa -i default \
+-r 30 \
  -c:v libx264 -r 30 -c:a flac $filename
  #-c:v ffvhuff -r 30 -c:a flac $filename
  #-f pulse -ac 1 -ar 44100 -i default \
