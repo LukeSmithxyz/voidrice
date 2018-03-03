04a2205aa82db54c61edc076240d93e64bbe5438
Luke Smith <luke@lukesmith.xyz>
2018-03-03 00:36:00 -0700

    lock more robust

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+# Take a screenshot:
 scrot /tmp/screen.png
-#convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-#convert /tmp/screen.png -paint 3 -modulate 80 /tmp/screen.png
-convert /tmp/screen.png -paint 3 /tmp/screen.png
+
+# Create a blur on the shot:
+convert /tmp/screen.png -paint 1 -swirl 360 /tmp/screen.png
+
+# Add the lock to the blurred image:
 [[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+
+# Pause music (mocp and mpd):
 mocp -P
-i3lock -u -e -i /tmp/screen.png
+mpc pause
+
+# Lock it up!
+i3lock -e -f -c 000000 -i /tmp/screen.png
+
+# If still locked after 20 seconds, turn off screen.
+sleep 20 && pgrep i3lock && xset dpms force off
