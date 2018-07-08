90f985a1f188c378b8481cb9a7ce0f8328311359
Luke Smith <luke@lukesmith.xyz>
2018-07-08 13:29:31 -0400

    pdf preview now first page

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -75,8 +75,9 @@ case "$extension" in
         try 7z -p l "$path" && { dump | trim; exit 0; } || exit 1;;
     # PDF documents:
     pdf)
-        try pdftotext -l 10 -nopgbrk -q "$path" - && \
-            { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
+	try pdftoppm -jpeg -singlefile "$path" "${cached//.jpg}" && exit 6 || exit 1;;
+        #try pdftotext -l 10 -nopgbrk -q "$path" - && \
+            #{ dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
     # BitTorrent Files
     torrent)
         try transmission-show "$path" && { dump | trim; exit 5; } || exit 1;;
