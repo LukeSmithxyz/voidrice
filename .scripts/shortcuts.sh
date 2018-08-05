da0a7dcd22d31dfe2416ac392d0791f75f0a50c3
Luke Smith <luke@lukesmith.xyz>
2018-08-05 19:54:28 -0400

    silly mix up

--- .scripts/shortcuts.sh
+++ .scripts/shortcuts.sh
@@ -26,8 +26,8 @@ rm -f "$shell_shortcuts" "$ranger_shortcuts" "$qute_shortcuts"
 
 # Format the `folders` file in the correct syntax and sent it to all three configs.
 sed "/^#/d" "$folders" | tee >(awk '{print "alias "$1"=\"cd "$2" && ls -a\""}' >> "$shell_shortcuts") \
-	>(awk '{print "config.bind(\";"$1"\", \"set downloads.location.directory "$2" ;; hint links download\")"}' >> "$ranger_shortcuts") \
-	| awk '{print "map g"$1" cd "$2"\nmap t"$1" tab_new "$2"\nmap m"$1" shell mv -v %s "$2"\nmap Y"$1" shell cp -rv %s "$2}' >> "$qute_shortcuts"
+	>(awk '{print "config.bind(\";"$1"\", \"set downloads.location.directory "$2" ;; hint links download\")"}' >> "$qute_shortcuts") \
+	| awk '{print "map g"$1" cd "$2"\nmap t"$1" tab_new "$2"\nmap m"$1" shell mv -v %s "$2"\nmap Y"$1" shell cp -rv %s "$2}' >> "$ranger_shortcuts"
 
 # Format the `configs` file in the correct syntax and sent it to both configs.
 sed "/^#/d" "$configs" | tee >(awk '{print "alias "$1"=\"$EDITOR "$2"\""}' >> "$shell_shortcuts") \
