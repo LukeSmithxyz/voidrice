40af68bf3801d5795b5d0b996f489d24c5a27eca
Luke Smith <luke@lukesmith.xyz>
2018-08-28 23:52:53 -0400

    better comment/blank-line-handling

--- .scripts/shortcuts.sh
+++ .scripts/shortcuts.sh
@@ -13,8 +13,8 @@ ranger_shortcuts="$HOME/.config/ranger/shortcuts.conf"
 qute_shortcuts="$HOME/.config/qutebrowser/shortcuts.py"
 
 # Download the shorcut files if not present.
-[[ ! -f "$folders" ]] && curl https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.scripts/folders > "$folders"
-[[ ! -f "$configs" ]] && curl https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.scripts/configs > "$configs"
+[ ! -f "$folders" ] && curl https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.scripts/folders > "$folders"
+[ ! -f "$configs" ] && curl https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.scripts/configs > "$configs"
 
 # Remove
 rm -f "$shell_shortcuts" "$ranger_shortcuts" "$qute_shortcuts"
@@ -25,10 +25,10 @@ rm -f "$shell_shortcuts" "$ranger_shortcuts" "$qute_shortcuts"
 (grep "config.source('shortcuts.py')" "$HOME/.config/qutebrowser/config.py")>/dev/null || echo "config.source('shortcuts.py')" >> "$HOME/.config/qutebrowser/config.py"
 
 # Format the `folders` file in the correct syntax and sent it to all three configs.
-sed "/^#/d" "$folders" | tee >(awk '{print "alias "$1"=\"cd "$2" && ls -a\""}' >> "$shell_shortcuts") \
+sed "s/#.*$//;/^$/d" "$folders" | tee >(awk '{print "alias "$1"=\"cd "$2" && ls -a\""}' >> "$shell_shortcuts") \
 	>(awk '{print "config.bind(\";"$1"\", \"set downloads.location.directory "$2" ;; hint links download\")"}' >> "$qute_shortcuts") \
 	| awk '{print "map g"$1" cd "$2"\nmap t"$1" tab_new "$2"\nmap m"$1" shell mv -v %s "$2"\nmap Y"$1" shell cp -rv %s "$2}' >> "$ranger_shortcuts"
 
 # Format the `configs` file in the correct syntax and sent it to both configs.
-sed "/^#/d" "$configs" | tee >(awk '{print "alias "$1"=\"$EDITOR "$2"\""}' >> "$shell_shortcuts") \
+sed "s/#.*$//;/^$/d"  "$configs" | tee >(awk '{print "alias "$1"=\"$EDITOR "$2"\""}' >> "$shell_shortcuts") \
 	| awk '{print "map "$1" shell $EDITOR "$2}' >> "$ranger_shortcuts"
