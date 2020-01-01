a27588e081e353afc11a4f1f2681b0cba65e3130
Vlad Doster <mvdoster@gmail.com>
2020-01-01 10:46:15 -0500

    Moving from i3 blocks to polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar mybar &
+
+echo "Polybar launched..."
