a6cefe52fc0c725713410dc7f9a98544a929c641
Vlad Doster <mvdoster@gmail.com>
2020-04-03 15:11:31 -0500

    added .screenlayout/ to dotfiles

---
+++ .screenlayout/dual_screens_nashville.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1366x768 --pos 1920x156 --rotate normal --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output HDMI-2 --off
