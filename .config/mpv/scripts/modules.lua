a08904d88a5dc413258c590ad794871cb3fc88e8
Wis <im@wis.am>
2020-04-09 04:35:03 +0300

    mpv config: use mpvSockets script to remove mpv wrappers/aliases (#544)
    * add scripts to mpv's cfg \w mpvSockets as 1st dependency and\or script
    
    * remove all usage of --input-ipc-server in all cmds
    
    * remove mpv alias that adds --input-ipc-server
    
    * fix pauseallmpv: correct paths to unix socket files
    
    * fix dependency-scripts loading so it works also when opening files off main disk

---
+++ .config/mpv/scripts/modules.lua
@@ -0,0 +1,3 @@
+local mpv_scripts_dir_path = os.getenv("HOME") ..  "/.config/mpv/scripts/"
+function load(relative_path) dofile(mpv_scripts_dir_path .. relative_path) end
+load("mpvSockets/mpvSockets.lua")
