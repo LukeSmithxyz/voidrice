0705097e5d44f8e7b0057ae75f7da93a9b232cb4
Wis <im@wis.am>
2021-09-13 16:55:50 +0300

    rename variable

--- .config/mpv/scripts/modules.lua
+++ .config/mpv/scripts/modules.lua
@@ -1,3 +1,3 @@
-local mpv_scripts_dir_path = require("mp").command_native({"expand-path", "~~/"})
-function load(relative_path) dofile(mpv_scripts_dir_path .. "/script_modules/" .. relative_path) end
+local mpv_config_dir_path = require("mp").command_native({"expand-path", "~~/"})
+function load(relative_path) dofile(mpv_config_dir_path .. "/script_modules/" .. relative_path) end
 load("mpvSockets/mpvSockets.lua")
