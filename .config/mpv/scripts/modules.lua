ec7995a6e9b1c80783d7962caeb3a2376e2bfe6b
HelionSmoker <82761116+HelionSmoker@users.noreply.github.com>
2021-11-23 21:35:07 +0200

    Delete .config/mpv directory

--- .config/mpv/scripts/modules.lua
+++
@@ -1,3 +0,0 @@
-local mpv_config_dir_path = require("mp").command_native({"expand-path", "~~/"})
-function load(relative_path) dofile(mpv_config_dir_path .. "/script_modules/" .. relative_path) end
-load("mpvSockets/mpvSockets.lua")
